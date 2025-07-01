4bf513eda6832ae545e71497a757ef63133178d6
João Pedro Voltolini <jptheshogun@gmail.com>
2025-07-01 11:13:43 -0300

    ajustes migrations aspnet identity

---
+++ Dima.Api/Data/Mappings/Identity/IdentityUserClaimMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings.Identity
+{
+    public class IdentityUserClaimMapping : IEntityTypeConfiguration<IdentityUserClaim<long>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserClaim<long>> builder)
+        {
+
+            builder.ToTable("IdentityClaim");
+            builder.HasKey(user => user.Id);
+            builder.Property(ct => ct.ClaimType).HasMaxLength(255);
+            builder.Property(cv => cv.ClaimValue).HasMaxLength(255);
+        }
+    }
+}
